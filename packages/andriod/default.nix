6ce644e0e44c5df5e9038e690c9c92d9defa38c4
Brian F <130115651+wvpianoman@users.noreply.github.com>
2023-09-05 11:12:31 -0400

    Update default.nix

--- packages/andriod/default.nix
+++ packages/andriod/default.nix
@@ -6,13 +6,13 @@
   #  Android Utilities
   #####################################
 
-      # adbfs-rootless          # Mount Android phones on Linux with adb, no root required
-      # haskellPackages.adb     # Android Debug Bridge (ADB) protocol
-      # android-tools           # Android SDK platform tools
-      # android-file-transfer   # Reliable MTP client with minimalistic UI
-      # droidcam                # Linux client for DroidCam app
-      # scrcpy                  # Display and control Android devices over USB or TCP/IP
-      # waydroid                # Waydroid is a container-based approach to boot a full Android system on a regular GNU/Linux system
+     adbfs-rootless          # Mount Android phones on Linux with adb, no root required
+     haskellPackages.adb     # Android Debug Bridge (ADB) protocol
+     android-tools           # Android SDK platform tools
+     android-file-transfer   # Reliable MTP client with minimalistic UI
+     droidcam                # Linux client for DroidCam app
+     scrcpy                  # Display and control Android devices over USB or TCP/IP
+     waydroid                # Waydroid is a container-based approach to boot a full Android system on a regular GNU/Linux system
     ];
   };
 }
